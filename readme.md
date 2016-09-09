a6b7231e05260192b072292b8b3e930e6d55e734
konstantinmagg <konstantin.magg@mailbox.org>
2016-09-09 14:00:18 +0200

    chapter 1 getting loaded.

--- readme.md
+++ readme.md
@@ -1,7 +1,10 @@
 # JavaScript Promises
 
 ## Example stoies
-[http://www.html5rocks.com/en/tutorials/es6/promises](http://www.html5rocks.com/en/tutorials/es6/promises)
+Source: [http://www.html5rocks.com/en/tutorials/es6/promises](http://www.html5rocks.com/en/tutorials/es6/promises)
 
 ## Example login
-[https://www.codeschool.com/screencasts/emberscreencasts-promises-basics](https://www.codeschool.com/screencasts/emberscreencasts-promises-basics)
+Source: [https://www.codeschool.com/screencasts/emberscreencasts-promises-basics](https://www.codeschool.com/screencasts/emberscreencasts-promises-basics)
+
+## Example foobar
+Source: [https://www.sitepoint.com/overview-javascript-promises/](https://www.sitepoint.com/overview-javascript-promises/)
--- script-login.js
+++ script-login.js
@@ -4,30 +4,68 @@ if (window.Promise) {
     console.error("Problem.");
 }
 
-var url = 'story.json';
-var foo = new Promise(function(resolve, reject) {
-    var request = new XMLHttpRequest();
+function addContent(snippet) {
+    document.getElementsByTagName('p')[0].innerHTML += snippet;
+}
 
-    request.open('GET', url);
-    request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-            resolve(request.response);
-        } else {
-            reject(Error(request.statusText));
-        }
-    };
+function clearContent() {
+    document.getElementsByTagName('p')[0].innerHTML = "";
+}
 
-    request.onerror = function() {
-        reject(Error('Error fetching data.'));
-    };
+function getAsPromise(url) {
+    var foo = new Promise(function(resolve, reject) {
+        var request = new XMLHttpRequest();
 
-    request.send();
-    console.log('Async request sent.');
-});
+        request.open('GET', url);
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                resolve(request.response);
+            } else {
+                reject(Error(request.statusText));
+            }
+        };
 
-foo.then(function(data) {
-    console.log('Promise fulfilled.');
-    document.getElementsByTagName('p')[0].textContent = data;
-}, function(error) {
-    console.error('Promise rejected: ' + error.message);
-});
+        request.onerror = function() {
+            reject(Error('Error fetching data.'));
+        };
+
+        request.send();
+        console.log('Async request sent.');
+    });
+
+    return foo;
+}
+
+console.log('Call #0');
+
+getAsPromise('story.json')
+    .then(function(data) {
+        console.log('Promise fulfilled.');
+        var content = JSON.parse(data);
+
+        return content; // return value to pass to next function as argument
+    })
+    .then(function(content) {
+        var url = content.chapterUrls[0]; // 'chapter-1.json'
+        console.log('Call #1');
+
+        return getAsPromise(url); // return promise to enqueue
+    })
+    .then(function(data) {
+        console.log('Promise fulfilled.');
+        var chapter = JSON.parse(data);
+        var template = '<h2>Chapter ' +
+            chapter.number +
+            ' &ndash; ' +
+            chapter.title +
+            '</h2><p>' +
+            chapter.content +
+            '</p>';
+        clearContent();
+        addContent(template);
+        // document.getElementsByTagName('p')[0].innerHTML = template;
+    })
+    .catch(function(error) {
+        // catches errors from any `getAsPromise()` call.
+        console.error('Promise rejected: ' + error.message);
+    });
